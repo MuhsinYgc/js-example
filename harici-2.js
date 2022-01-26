f98f9af1596d98192756dae9aead43b3aed480f2
MuhsinYgc <96091541+MuhsinYgc@users.noreply.github.com>
2022-01-26 13:49:13 +0300

    Delete harici-2.js

--- harici-2.js
+++
@@ -1,9 +0,0 @@
-//--döngüler
-// for(let i=0;i<=5;i++){
-//     console.log(i)
-//     console.log(`${i}*${i}= ${i*i}`)
-// }
-const names=['Mehmet','Muhsin','Yağcı','Ayşe','Ece','Yağcı']
-for (let i=0;i=6;i++){
-    console.log(names[i])
-}
